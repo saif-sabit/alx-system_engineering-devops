b76c79283b70f9c76b731175750f66902676a495
saif-sabit <saifsabit@gmail.com>
2023-09-22 18:31:45 +0300

    Task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create new file
 file{'/tmp/school':
-  ensure => file,
-  owner => 'www-data',
-  group => 'www-data',
-  privildge => '0744',
-  content => 'I love Puppet'
+  ensure  => 'present',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
 }
