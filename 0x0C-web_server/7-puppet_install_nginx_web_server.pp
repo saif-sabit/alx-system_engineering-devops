ee071968519d6ddab31ca40188449743a0c5cc91
saif-sabit <saifsabit@gmail.com>
2023-09-26 19:40:49 +0300

    Task 7 4

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+  # puppet
+  
+  package { 'nginx':
+    ensure => installed,
+  }
+
+  file { '/var/www/html/nginx':
+    ensure => 'directory',
+  }
+
+  exec { 'display_page':
+    command => '/usr/bin/sudo /bin/echo Hello World! > /var/www/html/nginx/index.nginx-debian.html',
+  }
+
+  exec { 'redirect_config':
+    command => '/usr/bin/sudo /bin/echo "location /redirect_me { return 301 http://tajba.tech; }" >> /etc/nginx/sites-available/default',
+  }
+
+  exec { 'server_start':
+    command => '/usr/bin/sudo /usr/sbin/service nginx start',
+  }
