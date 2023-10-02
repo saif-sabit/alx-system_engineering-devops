fcd287a62ae3cbeb8e39a407e19492ed1827777f
saif-sabit <saifsabit@gmail.com>
2023-10-02 19:23:37 +0300

    load balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,9 @@
+# This Puppet manifest configures Nginx to add a custom HTTP response header
+
+exec { 'http header':
+  command  => 'sudo apt-get update -y;
+	sudo apt-get install nginx -y;
+	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+	sudo service nginx restart',
+  provider => shell,
+}
