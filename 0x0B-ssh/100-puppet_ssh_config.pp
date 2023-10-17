e41eae12081a3c1bcb3d9f17854f1a42ecfc9acf
saif-sabit <saifsabit@gmail.com>
2023-10-17 12:16:06 +0300

    Task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,18 @@
+#!/usr/bin/env bash
 # Configuration
-file { '/etc/ssh':
-  ensure => directory,
+file { '/etc/ssh/ssh_config':
+  ensure => present,
 }
 
 # Enable private key and disable password authentication
 file_line { 'Turn off passwd auth':
   path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
+  match  => '#PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
   path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/school',
-  match => '^#?\s*IdentityFile\s+.+$',
-  match_for_absence => true,
+  match => '^#IdentityFile',
 }
