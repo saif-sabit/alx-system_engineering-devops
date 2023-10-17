4a0302231aaad2a822e5e36fdecc455da87bffe3
saif-sabit <saifsabit@gmail.com>
2023-10-17 12:27:28 +0300

    Task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,39 @@
-  # puppet
-  
+#!/usr/bin/env bash
+# Configure server using puppet
+
+# defines a Puppet class called nginx_server that 
+#  encapsulates the configuration for the Nginx server.
+class nginx_server {
   package { 'nginx':
-    ensure  => installed,
+    ensure => installed,
   }
 
-  file { '/var/www/html/nginx':
-    ensure => 'directory',
+#  manages the Nginx service.
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    require => Package['nginx'],
   }
+# manages the Nginx configuration file located at /etc/nginx/sites-available/default.
+  file { '/etc/nginx/sites-available/default':
+    ensure  => present,
+    content => "
+      server {
+        listen      80 default_server;
+        listen      [::]:80 default_server;
+        root        /var/www/html;
+        index       index.html index.htm;
 
-  exec { 'display_page':
-    command => '/usr/bin/sudo /bin/echo Hello World! > /var/www/html/nginx/index.nginx-debian.html',
-  }
+        location / {
+          return 200 'Hello World!';
+        }
 
-  exec { 'redirect_config':
-    command => '/usr/bin/sudo /bin/echo "location /redirect_me { return 301 http://tajba.tech; }" >> /etc/nginx/sites-available/default',
+        location /redirect_me {
+          return 301 http://cuberule.com/;
+        }
+      }
+    ",
+    notify => Service['nginx'],
   }
+}
 
-  exec { 'server_start':
-    command => '/usr/bin/sudo /usr/sbin/service nginx start',
-  }
