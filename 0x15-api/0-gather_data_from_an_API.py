2444517a79d55fc0c76e4ad2b568432aedc980a9
saif-sabit <saifsabit@gmail.com>
2023-10-23 09:06:00 -0700

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 import requests
 import sys
-import json
 if __name__ == '__main__':
         API = 'https://jsonplaceholder.typicode.com/users/';
         API2 = 'https://jsonplaceholder.typicode.com/todos/';
-        if (sys.argv > 1):
-                url = API+sys.argv[1]
+        if (len(sys.argv) > 1):
+                id = int(sys.argv[1])
+                url = API+str(id)
                 r = requests.get(url).json()
-                url2 = API+sys.argv[1]
+                url2 = API+str(id)
                 r2 = requests.get(url2).json()
-                if (r.status_code == 200 and r2.status_code == 200):
-                        name = r.get('name')
-                        total = len(r2)
-                        completed = list(filter(lambda x:x.get('userId') == sys.argv[1],r2))
-                        print(f"{0} is done with tasks({1}/{2}):".format(name, len(completed),total))
-                        for item in completed:
-                                print(f"\t {0}".format(item.get('title')))
+                name = r.get('name')
+                total = len(r2)
+                #completed = list(filter(lambda x:x.get('userId') == id,r2))
+                print("{0} is done with tasks({1}/{2}):".format(name,1,total))# len(completed),total))
+                for item in completed:
+                        print("\t {0}".format(item.get('title')))
                         
