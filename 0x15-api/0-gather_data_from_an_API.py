40beb8cfdc6d5f861d3bd07e7612f5e750099e4e
saif-sabit <saifsabit@gmail.com>
2023-10-24 06:54:52 -0700

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 import requests
 import sys
+'''
+Write a Python script that, using this REST API,
+ for a given employee ID
+, returns information about his/her TODO list progress
+'''
 if __name__ == '__main__':
-        API = 'https://jsonplaceholder.typicode.com/users/';
-        API2 = 'https://jsonplaceholder.typicode.com/todos/';
-        if (len(sys.argv) > 1):
-                id = int(sys.argv[1])
-                url = API+str(id)
-                r = requests.get(url).json()
-                url2 = API+str(id)
-                r2 = requests.get(url2).json()
-                name = r.get('name')
-                total = len(r2)
-                #completed = list(filter(lambda x:x.get('userId') == id,r2))
-                print("{0} is done with tasks({1}/{2}):".format(name,1,total))# len(completed),total))
-                for item in completed:
-                        print("\t {0}".format(item.get('title')))
-                        
+    API = 'https://jsonplaceholder.typicode.com/users/'
+    API2 = 'https://jsonplaceholder.typicode.com/todos/'
+    if (len(sys.argv) > 1):
+        id = int(sys.argv[1])
+        url = API+str(id)
+        r = requests.get(url).json()
+        url2 = API2
+        r2 = requests.get(url2).json()
+        name = r.get('name')
+        completed = []
+        userTasks = list(filter(lambda x: x.get('userId') == id, r2))
+        total = len(userTasks)
+        completed = list(filter(lambda x: x.get(
+                                              'completed') is True, userTasks))
+        print("{0} is done with tasks({1}/{2}):".format(
+                                                  name, len(completed), total))
+        for item in completed:
+            print("\t {0}".format(item.get('title')))
