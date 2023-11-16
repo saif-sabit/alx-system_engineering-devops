9a7caf329dac9becf8ac263b8e80e7122379ffe4
saif-sabit <saifsabit@gmail.com>
2023-11-16 17:06:36 +0300

    debug 3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#Strace is your friend
+
+exec {'fix apache.':
+  command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+}
