558698492f5d269cdae142efca9768751a3365ff
Saif sabit <39592784+saif-sabit@users.noreply.github.com>
2024-01-16 12:27:06 +0300

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-#Strace is your friend
-
-exec {'fix apache.':
-  command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
-  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-}
