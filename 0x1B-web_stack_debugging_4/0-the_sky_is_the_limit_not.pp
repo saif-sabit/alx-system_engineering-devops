8c88c76997b9e621b08991973aed79e8da453526
Saif sabit <39592784+saif-sabit@users.noreply.github.com>
2024-04-06 11:23:43 +0300

    Delete 0x1B-web_stack_debugging_4 directory
    Back to track

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++
@@ -1,3 +0,0 @@
-# Sky is the limit, let's bring that limit higher
-exec { '/usr/bin/env sed -i s/15/2000/ /etc/default/nginx': }
--> exec { '/usr/bin/env service nginx restart': }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,2 +0,0 @@
-# User limit
-exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
