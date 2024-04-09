b0d21d5b063259ccc725ab61133c47c3b45f8760
saif-sabit <saifsabit@gmail.com>
2024-04-09 13:19:48 +0300

    Advanced API 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    A function that queries the Reddit API and returns
+    the number of subscribers (not active users, total subscribers) 
+    for a given subreddit. If an invalid subreddit is given,
+    the function should return 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "user-Agent": "Tajba/API"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return 0
+    data = response.json().get("data")
+    return data.get("subscribers")
