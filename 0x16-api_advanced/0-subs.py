1588364ad34d81fd00f6e3a6983f1b047fa34d07
saif-sabit <saifsabit@gmail.com>
2024-04-09 13:32:47 +0300

    Advanced API 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 def number_of_subscribers(subreddit):
     """
     A function that queries the Reddit API and returns
-    the number of subscribers (not active users, total subscribers) 
+    the number of subscribers (not active users, total subscribers)
     for a given subreddit. If an invalid subreddit is given,
     the function should return 0.
     """
