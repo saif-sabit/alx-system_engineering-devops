18243a316856cf4db21de17429a326a84467d836
saif-sabit <saifsabit@gmail.com>
2024-08-15 10:42:33 +0300

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -20,5 +20,5 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    data = response.json().get("data")
-    return data.get("subscribers")
+    results = response.json().get("data")
+    return results.get("subscribers")
