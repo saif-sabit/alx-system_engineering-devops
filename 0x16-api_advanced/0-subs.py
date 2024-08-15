12525cfad75600b5754cdd00524ee0476be51c7f
saif-sabit <saifsabit@gmail.com>
2024-08-15 11:03:31 +0300

    update1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,7 +18,8 @@ def number_of_subscribers(subreddit):
         "user-Agent": "tajba/0.0.1"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code == 200:
+        results = response.json()
+        return results['data']['subscribers']
+    else:
         return 0
-    results = response.json()
-    return results['data']['subscribers']
