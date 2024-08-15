a44ebefe6d16aea305b814bf8618d71128641366
saif-sabit <saifsabit@gmail.com>
2024-08-15 11:24:48 +0300

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 """
 """
+import requests
 
 
 def top_ten(subreddit):
     """
-    doc 
+    doc
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
-        "user-Agent": "Tajba/API "
+        "user-Agent": "tajba/0.0.1 "
     }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Check if the subreddit is valid by looking at the status code
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        print(None)
